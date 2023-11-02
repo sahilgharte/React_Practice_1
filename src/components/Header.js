00dba2a0b91b06549ba1d2850daaca19282c2784
sahilgharte <sahilgharte4@gmail.com>
2023-11-02 07:56:32 +0530

    Added login logout useState variable and some styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,11 @@
 import React from "react";
 import { APP_LOGO } from "../utils/constants";
+import { useState } from "react";
 
 const Header = () =>{
+
+    let [btnName, setBtnName] = useState("Login");
+
     return (
         <div className="header">
             <div className="logo">
@@ -14,6 +18,11 @@ const Header = () =>{
                     <li>About Us</li>
                     <li>Contact Us</li>
                     <li>Cart</li>
+                    <button className="btn-login" onClick={ ()=> {
+                        btnName==="Logout"?btnName="Login":btnName="Logout"
+                        setBtnName(btnName);
+                        console.log(btnName);
+                        }}>{btnName}</button>
                 </ul>
             </div>
         </div>
