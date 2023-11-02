aeeea6448038d9d4a09560a1435b06cc85d79a60
sahilgharte <sahilgharte4@gmail.com>
2023-11-02 23:56:03 +0530

    Added search bar and Added search functionality using useState() React Hook

--- src/components/Body.js
+++ src/components/Body.js
@@ -22,6 +22,7 @@ const Body = () =>{
             // setListOfRestaurant(jso)
             console.log("response ---> " + JSON.stringify(json.data.cards[2].card.card.gridElements.infoWithStyle.restaurants));
             setListOfRestaurant(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+            setFilterListOfRestaurant(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
         } catch (error) {
             console.error('Error fetching data:', error);
         }
@@ -31,11 +32,35 @@ const Body = () =>{
 
     // 
      const [listOfRestaurant, setListOfRestaurant] = useState([]);
+     const [filterListOfRestaurant, setFilterListOfRestaurant] = useState([]);
+     const [searchFilter, setSearchFilter] = useState("");
 
     //  Conditional Rendering
         return listOfRestaurant.length === 0? <Shimmer/> :  (
         <div className="body">
             <div className="res-filter-btn">
+
+
+                <div className="search">
+                    <input type="text" className="search-box" value={searchFilter} onChange={(e)=>{
+                        setSearchFilter(e.target.value)
+                    
+                    }} />
+                    <button onClick={() => {
+                        console.log(searchFilter);
+
+                        const filteredRestaurant = listOfRestaurant.filter((res) => {
+                            return res.info.name.toLowerCase().includes(searchFilter.toLowerCase());   
+                        })
+
+                        setFilterListOfRestaurant(filteredRestaurant);
+
+
+                    }}>search</button>
+                </div>
+
+
+
                 <button className="filter-btn" onClick={() => {
                     const filterList = listOfRestaurant.filter((res) => res.info.avgRating > 4);
                     console.log(filterList);
@@ -47,7 +72,7 @@ const Body = () =>{
                         // we can use index a key but it is a very bad practice
                         // Unique Key >>>>>>>>> index
                         // Only use index if and only if there is no unique is
-                        listOfRestaurant.map((restaurant, index) => (
+                        filterListOfRestaurant.map((restaurant, index) => (
                             <RestaurantCard key={restaurant.info.id} resData = {restaurant} />
                         ))
                     }
