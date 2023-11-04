c0009b82cb669f1865cf590a000865c1b2c15907
sahilgharte <sahilgharte4@gmail.com>
2023-11-04 21:08:25 +0530

    Added childer routing using Outlet

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Body from "./components/Body";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
 // -----------------------
 
@@ -83,7 +83,7 @@ const AppComponent = () =>{
 return (
     <div className="app">
         <Header/>
-        <Body/>
+        <Outlet/>
     </div>
 );
 };
@@ -92,16 +92,22 @@ const appRouters = createBrowserRouter([
     {
         path: "/",
         element: <AppComponent/>,
+        children: [
+            {
+                path: "/",
+                element: <Body/>
+            },
+            {
+                path: "/about",
+                element: <About/>
+            },
+            {
+                path: "/contact",
+                element: <Contact/>
+            }
+        ],
         errorElement: <Error/>
     },
-    {
-        path: "/about",
-        element: <About/>
-    },
-    {
-        path: "/contact",
-        element: <Contact/>
-    }
 ]);
 
 
