fdc9bea3641608e8bc000c61d0e4d72345e3919f
sahilgharte <sahilgharte4@gmail.com>
2023-11-05 23:08:31 +0530

    Added a new Restaurant component to show restaurant menu

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,19 +1,43 @@
 import React from "react";
-import { useEffect } from "react";
+import { ReactDOM } from "react";
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 // https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.5204303&lng=73.8567437&restaurantId=456986
 
 
 const RestaurantMenu = () => {
 
-    // useEffect(()=> {
-    //     fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.5204303&lng=73.8567437&restaurantId=456986");
-    // }, [])
+    const [resInfo, setResInfo] = useState("");
+
+    useEffect(()=> {
+        fetchMenu()
+    }, [])
+
+    if (resInfo === null){
+        return <Shimmer/>
+    }
+
+    const fetchMenu = async () => {
+        const data = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.5204303&lng=73.8567437&restaurantId=456986");
+        const json = await data.json();
+
+
+
+        console.log("Menu API Called -> " + json.data)
+        setResInfo(json?.data?.cards[0]?.card?.card?.info)
+    }
+
+
+    const {name, areaName, avgRating, city, parentId, totalRatingsString, costForTwoMessage, cuisines} = resInfo
 
     return (
         <div>
             <div className="res-card-heading">
-                <h1>Name of the Restaurant</h1>
+                <h1>{name} , 🏠 {areaName}, {city}</h1>
+                <h3>Rating: {avgRating}⭐ , {totalRatingsString}</h3>
+                <h4>{costForTwoMessage}</h4>
+                <h4>{cuisines}</h4>
                 <h2>Menu</h2>
             </div>
 
