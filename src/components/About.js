6595728ec433bb4458b519eb442ccf95faae892c
sahilgharte <sahilgharte4@gmail.com>
2023-11-07 23:35:25 +0530

    Added props inside a classBased Component

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ const About = () => {
             <h1>About</h1>
             <h3>Hello guys this is my about us component ... Sooo elegent beutiful and :| </h3>
 
-            <UserClass/>
+            <UserClass name={"Sahil Gharte"} age= {"23"} phone={"9370778677"}/>
         </div>
 
     )
