e816307dfb63870cb261dbaddfe4e576479c5584
sahilgharte <sahilgharte4@gmail.com>
2023-11-07 23:39:46 +0530

    Added multiple cards inside the about us

--- src/components/About.js
+++ src/components/About.js
@@ -4,11 +4,17 @@ import UserClass from "./UserClass";
 const About = () => {
     return (
     
-        <div>
+        <div >
             <h1>About</h1>
             <h3>Hello guys this is my about us component ... Sooo elegent beutiful and :| </h3>
 
-            <UserClass name={"Sahil Gharte"} age= {"23"} phone={"9370778677"}/>
+            <div className="user-card-container">
+                <UserClass name={"Sahil Gharte"} age= {"23"} phone={"9370778677"}/>
+                <UserClass name={"Sahil Gharte"} age= {"23"} phone={"9370778677"}/>
+                <UserClass name={"Sahil Gharte"} age= {"23"} phone={"9370778677"}/>
+                <UserClass name={"Sahil Gharte"} age= {"23"} phone={"9370778677"}/>
+                <UserClass name={"Sahil Gharte"} age= {"23"} phone={"9370778677"}/>
+            </div>
         </div>
 
     )
