58d89f272754e2e9b794ecb56812b6a7f7dadb46
sahilgharte <sahilgharte4@gmail.com>
2023-11-07 23:34:34 +0530

    Added a classed based userClass under About us

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,16 @@
 import React from "react";
+import UserClass from "./UserClass";
 
 const About = () => {
     return (
+    
         <div>
             <h1>About</h1>
             <h3>Hello guys this is my about us component ... Sooo elegent beutiful and :| </h3>
+
+            <UserClass/>
         </div>
+
     )
 }
 export default About;
---
+++ src/components/UserClass.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { ReactDOM } from "react-router-dom";
+
+class UserClass extends React.Component{
+    
+    render(){
+        return (<div className="res-card">
+            <h2>Name: Sahil Gharte</h2>
+            <h3>Age: 23</h3>
+            <h3>Email: sahilgharte4@gmail.com</h3>
+            <h3>Phone: +91 9370778677</h3>
+        </div>)
+    }
+}
+
+export default UserClass;
